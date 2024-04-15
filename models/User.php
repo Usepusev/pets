12c039892e7039f619f4f76c48e0d821c1d4c8f7
senchrus <rsenchakov@gmail.com>
2024-04-15 18:54:22 +0300

    Half way done

---
+++ models/User.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $passwordconfirm
+ * @property int $role_id
+ *
+ * @property PetRequests[] $petRequests
+ * @property Role $role
+ */
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+{
+    public $passwordconfirm;
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'password', 'passwordconfirm'], 'required'],
+            [['role_id'], 'integer'],
+            [['name'], 'string', 'max' => 511],
+            [['email', 'password', 'passwordconfirm'], 'string', 'max' => 255],
+            [['email'], 'unique'],
+            [['email'], 'email'],
+            ['passwordconfirm', 'compare', 'compareAttribute' => 'password'],
+            [['role_id'], 'exist', 'skipOnError' => true, 'targetClass' => Role::class, 'targetAttribute' => ['role_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'password' => 'Password',
+            'passwordconfirm' => 'Password confirm',
+            'role_id' => 'Role ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[PetRequests]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPetRequests()
+    {
+        return $this->hasMany(PetRequests::class, ['user_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[Role]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRole()
+    {
+        return $this->hasOne(Role::class, ['id' => 'role_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    /**
+     * Finds user by email
+     *
+     * @param string $email
+     * @return static|null
+     */
+    public static function findByemail($email)
+    {
+        // [ 'key' => 'value' ]
+        return self::findOne(['email' => $email]);
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return null;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+}
